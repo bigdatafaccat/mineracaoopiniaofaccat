95011cfc417e7a8c635a7a4297e4652916af4e23
rweber <rodrigoantonioweber@hotmail.com>
2017-07-07 13:12:46 -0300

    Extração de postagens, seus comentários

--- extracao/extrator/index.py
+++ extracao/extrator/index.py
@@ -1,9 +1,28 @@
 import facebook
 import requests
 
-graph = facebook.GraphAPI("EAACEdEose0cBAJPSo4DZCGlaZBfGk56aAvwnAIeSG3nOju3sebleeuxJw58ALoT3TZAxkJYC4Ia5VwnstZCdu72ZB7wnR1XtAOPBHQoXsrjbRiDfg4ko8b6M78ed9R35g5zblZBUm3DZBiPlZCtwl6J9wpvPNnqvBcWsFOCmWWFQKt8vBek8cfDcFrDigcnv1dwZD")
-profile = graph.get_object(id="160317500766953_1059336457531715", fields="from,comments.fields(likes),message,likes")
+graph = facebook.GraphAPI("") #Page access token
+data = graph.get_object("160317500766953/feed?fields=id")
 
-r = requests.post("http://localhost:3000/api/users", data=profile)
+# fala igrejinha: 160317500766953
 
-print(r.status_code)
+while(True):
+    try:
+        d = data["data"]
+        g = data["paging"]
+
+        for x in d:
+            data = graph.get_object(
+                x["id"] + "?fields=from,comments.fields(like),message,likes")
+
+            result = requests.post(
+                "http://localhost:3000/api/users", data=data)
+
+            print("Http status: " + str(result.status_code))
+
+        print('Next page')
+
+        data = requests.get(g["next"]).json()
+    except KeyError:
+        print('error')
+        break;
