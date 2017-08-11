56abe7443f7397516da844308cef986fd5f10061
rweber <rodrigoantonioweber@hotmail.com>
2017-08-11 13:01:28 -0300

    Criado arquivo de configuração e documentação

--- extrator/index.py
+++ extrator/index.py
@@ -1,126 +1,184 @@
-import facebook
-import requests
 import json
 import datetime
-
-graph = facebook.GraphAPI("", version="2.9")  # Page access token
-feeds = graph.get_object("160317500766953/feed?fields=created_time,id")
-
-longLiveToken = graph.extend_access_token(
-    1917780418489769, "f7d69cbb36e1af37f04d3c723e4dc2b0")
-
-graph = facebook.GraphAPI(longLiveToken["access_token"], version="2.9")
-
-# fala igrejinha: 160317500766953
+import traceback
+import requests
 
 
-def createQueryString(groupId):
-    #"21785951839_10155897661861840"
-    text = groupId + "?fields="
-    text += "admin_creator,application,backdated_time,"
-    text += "call_to_action,child_attachments,actions,"
+def create_url_post(post_id, config):
+    """
+    Cria a url do post com os campos desejados
+    """
+    text = "https://graph.facebook.com/"
+    text += config["graph_api_version"]
+    text += "/" + post_id + "?fields="
+    text += "id, message, admin_creator, backdated_time,"
     text += "caption,coordinates,created_time,description,"
     text += "feed_targeting,from,event,icon,is_popular,link,"
-    text += "message,message_tags,name,object_id,type,shares,"
-    text += "story,source,properties,place,target,targeting,via,"
+    text += "message_tags,name,object_id,type,shares,"
+    text += "story,source,properties,place,target,targeting,"
     text += "status_type,comments.fields(comment_count,from,id,like_count,"
     text += "permalink_url,created_time,message,comments.fields(comment_count,"
-    text += "from,id,like_count,permalink_url,created_time,message,"
+    text += "from,id,like_count,created_time,message,"
     text += "reactions.fields(id,name,type,username,profile_type))),"
-    text += "reactions.fields(id,name,type,username,profile_type),"
-    text += "sharedposts,story_tags"
+    text += "reactions.fields(id,name,type,username,profile_type), sharedposts"
+    text += "&access_token=" + config["access_token"]
 
     return text
 
 
-def seekMoreData(data):
-    listData = []
+def create_url_feed(config):
+    """
+    Cria a url dos feeds da página
+    """
+    text = "https://graph.facebook.com/"
+    text += config["graph_api_version"]
+    text += "/"
+    text += config["page_id"]
+    text += "/"
+    text += "feed?fields=created_time,id,permalink_url"
+    text += "&access_token="
+    text += config["access_token"]
+
+    return text
+
+
+def seek_more_data(data):
+    """
+    Busca os dados por enquanto que tiver paginação
+    """
+    list_data = []
 
     paging = data["paging"]
 
     while "next" in paging:
-        nextUrl = paging["next"]
-        jsonData = requests.get(nextUrl).json()
+        next_url = paging["next"]
 
-        for jsonItem in jsonData["data"]:
-            listData.append(jsonItem)
+        json_data = requests.get(next_url, stream=True).json()
 
-        paging = jsonData["paging"]
+        for json_item in json_data["data"]:
+            list_data.append(json_item)
 
-    return listData
+        paging = json_data["paging"]
+
+    return list_data
 
 
-while(True):
-    try:
-        feedsData = feeds["data"]
-        feedsPaging = feeds["paging"]
+def read_config_file():
+    """
+    Lê o arquivo de configuração
+    """
+    with open('config.json') as json_data:
+        data = json.load(json_data)
 
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    return data
 
-        postLimitDate = datetime.datetime(2017, 8, 4)
 
-        limitReached = False
+def main():
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-        for feed in feedsData:
-            postDate = datetime.datetime.strptime(
-                feed["created_time"], '%Y-%m-%dT%H:%M:%S+0000')
+    config = read_config_file()
 
-            if postLimitDate.date() > postDate.date():
-                limitReached = True
+    post_limit_date = datetime.datetime.strptime(
+        config["post_limit_date"], '%Y-%m-%d')
+
+    limit_reached = False
+
+    feed_url = create_url_feed(config)
+
+    feeds = requests.get(feed_url, stream=True).json()
+
+    while True:
+        try:
+            feeds_data = feeds["data"]
+            feeds_paging = feeds["paging"]
+
+            for feed in feeds_data:
+                post_date = datetime.datetime.strptime(
+                    feed["created_time"], '%Y-%m-%dT%H:%M:%S+0000')
+
+                if post_limit_date.date() > post_date.date():
+                    limit_reached = True
+                    break
+
+                print(feed["permalink_url"])
+
+                post_url = create_url_post(feed["id"], config)
+
+                graph_object = requests.get(post_url, stream=True).json()
+
+                if "error" in graph_object:
+                    print(graph_object["error"]["message"])
+                    continue
+
+                if "reactions" in graph_object:
+                    post_reactions = seek_more_data(graph_object["reactions"])
+
+                    for record in post_reactions:
+                        graph_object["reactions"]["data"].append(record)
+
+                if "comments" in graph_object:
+                    post_comments = seek_more_data(graph_object["comments"])
+
+                    for record in post_comments:
+                        graph_object["comments"]["data"].append(record)
+
+                    for pC in graph_object["comments"]["data"]:
+                        if "comments" in pC:
+                            if "paging" in pC["comments"]:
+                                post_comments_of_comments = seek_more_data(
+                                    pC["comments"])
+
+                                for post in post_comments_of_comments:
+                                    pC["comments"]["data"].append(post)
+
+                        if "reactions" in pC:
+                            if "paging" in pC["reactions"]:
+                                post_reactions_of_comments = seek_more_data(
+                                    pC["reactions"])
+
+                                for post in post_reactions_of_comments:
+                                    pC["reactions"]["data"].append(post)
+
+                    if not "data" in graph_object["comments"]:
+                        break
+
+                    for comments in graph_object["comments"]["data"]:
+                        if not "comments" in comments:
+                            break
+
+                        for comment_of_comments in comments["comments"]["data"]:
+                            if not "reactions" in comment_of_comments:
+                                break
+
+                            if not "next" in comment_of_comments["reactions"]["paging"]:
+                                break
+
+                            comments_of_comments_reactions = seek_more_data(
+                                comment_of_comments["reactions"])
+
+                            for reactions in comments_of_comments_reactions:
+                                reactions["reactions"]["data"].append(
+                                    reactions)
+
+                result = requests.post(config["webservice_url"],
+                                       data=json.dumps(graph_object), headers=headers, stream=True)
+
+                print("Http status: " + str(result.status_code))
+
+            if limit_reached:
+                print('Deadline reached')
                 break
 
-            queryString = createQueryString(feed["id"])
+            if not "next" in feeds_paging:
+                break
 
-            graphObject = graph.get_object(queryString)
+            print('Next page')
 
-            if "reactions" in graphObject:
-                postReactions = seekMoreData(graphObject["reactions"])
-
-                for record in postReactions:
-                    graphObject["reactions"]["data"].append(record)
-
-            if "comments" in graphObject:
-                postComments = seekMoreData(graphObject["comments"])
-
-                for record in postComments:
-                    graphObject["comments"]["data"].append(record)
-
-                for pC in graphObject["comments"]["data"]:
-                    if "comments" in pC:
-                        if "paging" in pC["comments"]:
-                            postCommentsOfComments = seekMoreData(
-                                pC["comments"])
-                            for post in postCommentsOfComments:
-                                pC["comments"]["data"].append(post)
-                    if "reactions" in pC:
-                        if "paging" in pC["reactions"]:
-                            postReactionsOfComments = seekMoreData(
-                                pC["reactions"])
-                            for post in postReactionsOfComments:
-                                pC["reactions"]["data"].append(post)
-
-                if "data" in graphObject["comments"]:
-                    for c in graphObject["comments"]["data"]:
-                        if "comments" in c:
-                            for cc in c["comments"]["data"]:
-                                if "reactions" in cc:
-                                    if "next" in cc["reactions"]["paging"]:
-                                        postCommentsOfCommentsReactions = seekMoreData(
-                                            cc["reactions"])
-                                        for ccr in postCommentsOfCommentsReactions:
-                                            cc["reactions"]["data"].append(ccr)
-
-            result = requests.post("http://localhost:3000/api/posts",
-                                   data=json.dumps(graphObject), headers=headers)
-
-            print("Http status: " + str(result.status_code))
-
-        print('Next page')
-
-        if limitReached:
+            feeds = requests.get(feeds_paging["next"], stream=True).json()
+        except Exception:
+            traceback.print_exc()
             break
 
-        feedsData = requests.get(feedsPaging["next"]).json()
-    except Exception as e:
-        print('Error: ' + str(e))
-        break
+
+if __name__ == "__main__":
+    main()
--- webservice/README.md
+++ webservice/README.md
@@ -1,17 +1,28 @@
-Andromeda
-==================================
 
 Webservice para salvar dados obtidos do Facebook.
+=
+
+### Http GET: api/posts
+Buscar todos os posts salvos no banco de dados
+
+### Http GET: api/posts/pretty
+Busca todos os posts salvos e os exibe de uma maneira mais amigável
 
 
-- Suporta GET, POST e DELETE
-- Utilizada Nodejs e Mongodb
+### Http POST: api/posts: 
+Inclui um post no banco de dados
+
+
+### Http DELETE: api/posts/delete
+Passando o id do registro para ser excluído
+
+### Http DELETE: api/posts/deleteAll
+Exclui todos os posts do banco de dados
+
+## Instalar
+
+Todas as dependências estão no arquivos package.json, é necessário possuir o node.js instalado e executar **npm install**.
 
 ## Como usar
 
-- GET, POST e DELETE para pessoas em /api/users
-
-TODO
----------------
-
-- Autenticação
+Executar **npm run start** para subir o servidor.
